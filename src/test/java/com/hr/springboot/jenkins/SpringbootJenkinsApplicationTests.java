6826122b52ad7fcf4efef34fcfc9c444a614ef95
Himanshu Bhusan Rath <himanshubhusanrath@admins-MacBook-Pro-4.local>
2022-07-11 16:37:35 +0530

    update

--- src/test/java/com/hr/springboot/jenkins/SpringbootJenkinsApplicationTests.java
+++ src/test/java/com/hr/springboot/jenkins/SpringbootJenkinsApplicationTests.java
@@ -3,13 +3,13 @@ package com.hr.springboot.jenkins;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest 
 class SpringbootJenkinsApplicationTests {
 	
